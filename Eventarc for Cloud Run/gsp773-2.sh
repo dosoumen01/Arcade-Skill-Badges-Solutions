934b8942815b1f0e52c520d141acf8b7ae10dce3
Soumen Kumar <131024573+dosoumen01@users.noreply.github.com>
2024-07-07 17:03:54 +0530

    Update gsp773-2.sh

--- Eventarc for Cloud Run/gsp773-2.sh
+++ Eventarc for Cloud Run/gsp773-2.sh
@@ -2,13 +2,14 @@ echo "Hello World" > random.txt
 
 gsutil cp random.txt gs://${BUCKET_NAME}/random.txt
 
+gsutil cp random.txt gs://${BUCKET_NAME}/random.txt
+
+
 export SERVICE_NAME=event-display
 
 export PROJECT_NUMBER="$(gcloud projects list \
   --filter=$(gcloud config get-value project) \
   --format='value(PROJECT_NUMBER)')"
-  
-gcloud eventarc providers describe cloudaudit.googleapis.com
 
 gcloud eventarc triggers create trigger-auditlog \
   --destination-run-service=${SERVICE_NAME} \
